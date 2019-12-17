f5830814eab7c05b479ccef417ef68aeb41c2192
Joshua Sanders <ninjosh2@gmail.com>
2019-12-17 07:28:06 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -14,7 +14,10 @@ def sort_array_char_count(array)
   end
 end 
 
-def swap_elements(array) 
+def swap_elements(array, index, new_index)
+  array[index], array[new_index] = array[new_index], array[index]
+  return array
+end 
   
 
 
